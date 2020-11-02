6e788527822b7b532100c2065c9637e43962147b
Павел Михно <mikhno.pashaki154@gmail.com>
2020-11-02 23:14:17 +0200

    Added TripleDES and RSACng Encoding

---
+++ Cryptography_LB3/Actions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptography_LB3
+{
+    public enum Actions
+    {
+        Uknown = 0,
+        EncryptTripleDES = 1,
+        RSACng = 2,
+        DSA = 3,
+        SHA384Managed = 4,
+        DecryptTripleDES = 5,
+        Exit = 6
+    }
+}
---
+++ Cryptography_LB3/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Cryptography_LB3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                bool isExit = false;
+
+                Console.WriteLine("Please select an action:");
+                Console.WriteLine("1) Encrypt using TripleDES;\n2) Encrypt using RSACng;\n3) Create DSA Sign;\n4) Get SHA384Managed Hash;\n5) Decrypt TripleDES file using key file\n6) Exit");
+                var selected = Console.ReadLine();
+                int action;
+                var isValid = int.TryParse(selected, out action);
+
+                if (!isValid)
+                {
+                    Console.WriteLine("Ivalid input!");
+                }
+
+                switch ((Actions)action)
+                {
+                    case Actions.EncryptTripleDES:
+                        Console.WriteLine("===============================================================================");
+                        Console.WriteLine("Please enter value that will be encrypted:");
+                        var value = Console.ReadLine();
+                        TripleDesService.EncryptTripleDES(value);
+                        Console.WriteLine("===============================================================================\n");
+                        break;
+                    case Actions.DecryptTripleDES:
+                        ProcessDecryptTripleDESAction();
+                        break;
+                    case Actions.Exit:
+                        isExit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Ivalid input!");
+                        break;
+                }
+
+                if (isExit)
+                {
+                    break;
+                }
+                continue;
+            }
+        }
+
+        public static void ProcessDecryptTripleDESAction()
+        {
+            Console.WriteLine("===============================================================================");
+            Console.WriteLine("Please enter path to encrypted file:");
+            byte[] encriptedFileBytes;
+            while (true)
+            {
+                var encryptedPath = Console.ReadLine();
+                if (!File.Exists(encryptedPath))
+                {
+                    Console.WriteLine("Invalid file path! try again");
+                    continue;
+                }
+                encriptedFileBytes = File.ReadAllBytes(encryptedPath);
+                break;
+            }
+            Console.WriteLine("Please enter path to key file:");
+            byte[] keyFileBytes;
+            while (true)
+            {
+                var keyFilePath = Console.ReadLine();
+                if (!File.Exists(keyFilePath))
+                {
+                    Console.WriteLine("Invalid file path! try again");
+                    continue;
+                }
+                keyFileBytes = File.ReadAllBytes(keyFilePath);
+                break;
+            }
+            Console.WriteLine("Please enter path to initialization vector file:");
+            byte[] vectorFileBytes;
+            while (true)
+            {
+                var vectorFilePath = Console.ReadLine();
+                if (!File.Exists(vectorFilePath))
+                {
+                    Console.WriteLine("Invalid file path! try again");
+                    continue;
+                }
+                vectorFileBytes = File.ReadAllBytes(vectorFilePath);
+                break;
+            }
+
+            TripleDesService.DecryptTripleDES(encriptedFileBytes, keyFileBytes, vectorFileBytes);
+            Console.WriteLine("===============================================================================\n");
+        }
+    }
+}
